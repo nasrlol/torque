380663c7e9a52e37692946fce35ca50888233752
Abdellah El Morabit <nsrddyn@gmail.com>
2025-11-15 23:17:36 +0100

    feature: boilerplate FPU
    boilerplate classes for stressing the FPU, will be extended in the
    future, still discovering, what does what and what stresses what

---
+++ src/main/scala/com/nsrddyn/cpu/FPU/CholeskyDecomposition.scala
@@ -0,0 +1,28 @@
+package com.nsrddyn
+
+
+class CholeskyDecomposition {
+
+  /*
+   *  Floating point operation to stress the cpu
+   *  Calculate the number of KFLOPS / FLOPS
+   *  implementation of the Cholesky decomposition
+   *  More information on the Cholesky decomposition at: 
+   *  https://en.wikipedia.org/wiki/Cholesky_decomposition
+   *  
+   *  Linpack uses the cholesky decomposition
+   *  https://www.netlib.org/linpack/
+   *
+   *
+   */ 
+
+  def choleskyDecomposition(n: Int): Unit =   {
+
+    for (w <- 0 to n) {
+
+
+    }
+  }
+
+  
+}
