ff2942a46d943f2ef30ac74ef6ea75992a3ac26c
Abdellah El Morabit <nsrddyn@gmail.com>
2025-11-28 23:20:58 +0100

    feature: succesfully allocated memory on the heap ( or the off heap ) not sure what happened lol

--- src/main/scala/main/domain/MemoryOperations.scala
+++ src/main/scala/main/domain/MemoryOperations.scala
@@ -3,47 +3,52 @@ package main.domain
 import main.services._
 
 import scala.util.hashing
-import scala.util.hashing.MurmurHash3 import scala.math._
+import scala.util.hashing.MurmurHash3 
 import scala.collection.immutable.ListSet
-import java.nio.ByteBuffer
+import scala.math._
+import java.lang.foreign._
+import java.lang.foreign.MemorySegment
+import java.lang.foreign.ValueLayout
+import scala.util.Using
 
-/**
- *
- * Large memory object to allocate on the heap 
- * This is an implementation by me 
- * A method to do this bassically an Array with large bytes allocated to it
- *
- * */
+object Memory {
 
 
-class MemoryObject {
+  /**
+   * Large memory object to allocate on the heap 
+   * This is an implementation by me 
+   * A method to do this bassically an Array with large bytes allocated to it
+   * */
 
-  object size {
-    val size: Int = 10 * 1024 * 1024
+  class MemoryObject {
+
+    // TODO: create a large sized object
+
   }
 
-  val buffer: Vector[size.type] = Vector(1, 1) 
-}
+  /*
+   * Using java.nio.ByteBuffer
+   * Allocate memory then free memory
+   * for stressing the memory we want to do some heavy heap allocations back and forth
+   * --------- 
+   *  not anymore
+   *  switched to java.lang.foreign
+   *
+   * https://docs.oracle.com/en/java/javase/22/docs/api/java.base/java/lang/foreign/package-summary.html
+   * */
+
+  class MemoryAllocater(arena: Arena)  {
 
 
+    def run(): Unit = moveToMemory()
 
-/*
- * Using java.nio.ByteBuffer
- * Allocate memory then free memory
- * for stressing the memory we want to do some heavy heap allocations back and forth
- * 
- *
- * */
+    def moveToMemory(): Unit = {
 
-class MemoryAllocater extends ByteBuffer {
+      val memseg = arena.allocate(10 * 4)
+      // not memseg.setAtIndex like in the docs :(
+      for i <- 0 to 10 do memseg.set(ValueLayout.JAVA_INT, i, i)
 
-  def run(): Unit = println("stressing memory") 
-
-
-  def moveToMemory(): Unit ={
+    } 
 
   } 
-} 
-
-
-
+}
